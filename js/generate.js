4848a600870c9b5619bf4a75414328d8fc8d5321
Petros Kalogiannakis <kalpetros@gmail.com>
2015-08-24 21:39:43 +0300

    Minor UI fixes

--- js/generate.js
+++ js/generate.js
@@ -1,4 +1,5 @@
-// Copyright 2011 Gavin Wahl, Colin Thomas-Arnold, Fusionbox. Modified by Petros Kalogiannakis. All rights reserved.
+// Copyright 2011 Gavin Wahl, Colin Thomas-Arnold, Fusionbox. 
+// Modified by Petros Kalogiannakis. All rights reserved.
 
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:
