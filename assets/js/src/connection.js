c614a388963af1bbbee7dad62c7d19a73741eccd
Petros Kalogiannakis <kalpetros@gmail.com>
2017-05-21 03:46:48 +0300

    fix: minor fix

--- assets/js/src/connection.js
+++ assets/js/src/connection.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
         if (navigator.onLine) {
 	    var protocol = "You appear to be online.";
 	    if (document.location.protocol === 'https:') {
-		$('.connection').html('<a class="grey-text"><i class="fa fa-lock grey-text" aria-hidden="true"></i> Secure connection</a>');
+		$('.connection').html('<a class="teal-text text-accent-3"><i class="fa fa-lock teal-text text-accent-3" aria-hidden="true"></i> Secure connection</a>');
 		protocol += " You are using https";
 	    } else {
-		$('.connection').html('<a class="grey-text"><i class="fa fa-unlock-alt grey-text" aria-hidden="true"></i> Unsecure connection</a>');
+		$('.connection').html('<a class="red-text text-accent-3"><i class="fa fa-unlock-alt red-text text-accent-3" aria-hidden="true"></i> Unsecure connection</a>');
 		protocol += " Switch to https.";
 	    }
 	    
@@ -23,10 +23,10 @@ $(document).ready(function() {
     window.addEventListener('online', function() {
 	var protocol = "You appear to be online.";
     	if (document.location.protocol === 'https:') {
-	    $('.connection').html('<a class="grey-text"><i class="fa fa-lock grey-text" aria-hidden="true"></i> Secure connection</a>');
+	    $('.connection').html('<a class="teal-text text-accent-3"><i class="fa fa-lock teal-text text-accent-3" aria-hidden="true"></i> Secure connection</a>');
 	    protocol += " You are using https";
 	} else {
-	    $('.connection').html('<a class="grey-text"><i class="fa fa-unlock-alt grey-text" aria-hidden="true"></i> Unsecure connection</a>');
+	    $('.connection').html('<a class="red-text text-accent-3"><i class="fa fa-unlock-alt red-text text-accent-3" aria-hidden="true"></i> Unsecure connection</a>');
 	    protocol += " Switch to https.";
 	}
 	$('.tooltipped').tooltip({position: 'right',
